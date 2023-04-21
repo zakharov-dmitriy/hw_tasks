b62da00beaeb1615a456677604eca595cd2d3e53
Dmitrii <you@example.com>
2023-04-21 10:21:43 +0700

    update script.sh

--- Terminal/script.sh
+++ Terminal/script.sh
@@ -7,6 +7,6 @@ for i in {1..3}; do touch file$i.txt; done
 for i in 0 1; do touch file-json$i.json; done
 cd ..
 ls -la
-mv dir3/file1.txt dir3/file-json1.json dir2
+mv dir3/{file1.txt,file-json1.json} dir2
 
 echo "The script was executed successfully"
